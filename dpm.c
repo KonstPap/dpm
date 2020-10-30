f36f8fdd4126cbfa734f91448488a96e6df76993
KonstPap <32640290+KonstPap@users.noreply.github.com>
2020-10-30 15:43:25 +0200

    Update dpm.c

--- dpm.c
+++ dpm.c
@@ -9,3 +9,13 @@ int main()
     if(mon<1 || mon>12)
         printf("There is not such a month\n");
     else
+    {
+        if(mon==1||mon==3||mon==5||mon==7||mon==8||mon==10||mon==12)
+            printf("Days=31\n");
+        else if(mon==4||mon==6||mon==9||mon==11)
+            printf("Days=30\n");
+        else
+            printf("Days=28\n");
+    }
+    return 0;
+}
